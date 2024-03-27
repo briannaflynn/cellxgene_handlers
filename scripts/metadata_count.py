41f9ab3177906476e7f64943a92b10b507c6459d
briannaflynn <brianna.flynn@utexas.edu>
2024-03-27 07:56:35 -0500

    class to aggregate metadata jsons, option to include cell type in output

--- scripts/metadata_count.py
+++ scripts/metadata_count.py
@@ -35,7 +35,6 @@ class MetadataProcessor:
                 data = json.load(file)
                 self.process_dict(data)
 
-        # Convert each level's Counter object to a DataFrame
         levels_dfs = []
         for level_count in self.levels_counts:
             df = pd.DataFrame(list(level_count.items()), columns=['Key', 'Count'])
@@ -43,12 +42,91 @@ class MetadataProcessor:
 
         return levels_dfs
 
+    def aggregate_datasets_info(self, json_file_paths, cell_type = False, verbose = True, output_csv_path='../data/aggregated_metadata_json.csv'):
+
+        def fill_missing_values(df):
+            
+            columns_to_fill = ['development_stage', 'disease', 'organism']
+            last_valid_values = {col: None for col in columns_to_fill}
+
+            for i, row in df.iterrows():
+                for col in columns_to_fill:
+                    if pd.notnull(row[col]) and row[col].strip():
+                        last_valid_values[col] = row[col]
+                    elif last_valid_values[col] is not None:
+                        df.at[i, col] = last_valid_values[col]
+
+                if i < len(df) - 1 and df.at[i, 'dataset_id'] != df.at[i + 1, 'dataset_id']:
+                    last_valid_values = {col: None for col in columns_to_fill}
+
+            return df
+
+        aggregated_data = []
+
+        for file_path in json_file_paths:
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                collection_id = data['collection_id']
+                print(f"Processing {collection_id}")
+                print(file_path)
+                for dataset in data['datasets']:
+
+                    dataset_id = dataset['dataset_id']
+                    raw_data_location = dataset['raw_data_location']
+
+                    if cell_type:
+                        developmental_stages = dataset['development_stage']
+                        diseases = dataset['disease']
+                        organisms = dataset['organism']
+                        cells = dataset['cell_type']
+
+                        max_length = max(len(developmental_stages), len(diseases), len(organisms), len(cells))
+                        
+                        for i in range(max_length):
+                            dev_stage_label = developmental_stages[i].get('label', '') if i < len(developmental_stages) else ''
+                            disease_label = diseases[i].get('label', '') if i < len(diseases) else ''
+                            organism_label = organisms[i].get('label', '') if i < len(organisms) else ''
+                            cell_label = cells[i].get('label', '') if i < len(cells) else ''
+
+                            aggregated_data.append({
+                                'collection_id':collection_id,
+                                'dataset_id':dataset_id,
+                                'development_stage':dev_stage_label,
+                                'disease':disease_label,
+                                'organism':organism_label,
+                                'raw_data_location':raw_data_location,
+                                'cell_type':cell_label
+                            })
+                    else:
+                        developmental_stages = dataset['development_stage']
+                        diseases = dataset['disease']
+                        organisms = dataset['organism']
+
+                        max_length = max(len(developmental_stages), len(diseases), len(organisms))
+                        
+                        for i in range(max_length):
+                            dev_stage_label = developmental_stages[i].get('label', '') if i < len(developmental_stages) else ''
+                            disease_label = diseases[i].get('label', '') if i < len(diseases) else ''
+                            organism_label = organisms[i].get('label', '') if i < len(organisms) else ''
+
+                            aggregated_data.append({
+                                'collection_id':collection_id,
+                                'dataset_id':dataset_id,
+                                'development_stage':dev_stage_label,
+                                'disease':disease_label,
+                                'organism':organism_label,
+                                'raw_data_location':raw_data_location
+                            })
+
+        df = fill_missing_values(pd.DataFrame(aggregated_data))
+        
+        if verbose:
+            print(df)
+
+        df.to_csv(output_csv_path, index=False)
+
 data_processor = MetadataProcessor()
 json_file_paths = data_processor.read_single_column_file_to_list('../data/metadata_files.txt')
-levels_dfs = data_processor.count_items_in_json_files(json_file_paths)
 
-for idx, df in enumerate(levels_dfs):
-    print(f"DataFrame for Level {idx}:")
-    print(df, "\n")
-    # Save each level's DataFrame to a CSV file
-    df.to_csv(f"level_{idx}.csv")
+data_processor.aggregate_datasets_info(json_file_paths)
+data_processor.aggregate_datasets_info(json_file_paths, cell_type=True, output_csv_path = '../data/aggregated_metadata_json_with_celltype.csv')
