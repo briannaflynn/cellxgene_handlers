5beec56a679ba1f9f5b08e5cc3572df47e36c97e
briannaflynn <brianna.flynn@utexas.edu>
2024-07-08 08:48:26 -0500

    add raw data matrix to synthetic anndata object

--- scripts/synthetic_data_generator.py
+++ scripts/synthetic_data_generator.py
@@ -15,6 +15,17 @@ def generate_synthetic_anndata(num_cells:int, num_genes:int, sparsity:float=0.1,
     # create anndata object
     adata = anndata.AnnData(X=data_sparse)
 
+    # add raw data matrix to anndata object
+    raw_data = np.random.poisson(1, (num_cells, num_genes))
+    raw_mask = np.random.binomial(1, 1 - sparsity, raw_data.shape).astype(bool)
+    raw_data[~raw_mask] = 0
+    raw_data_sparse = sp.csr_matrix(raw_data)
+
+    adata.raw = anndata.AnnData(X=raw_data_sparse)
+
+    adata.raw.var['gene_id'] = adata.var['gene_id']
+    adata.raw.var_names = adata.var_names
+
     adata.obs['cell_id'] = [f'cell_{i}' for i in range(num_cells)]
     adata.var['gene_id'] = [f'gene_{i}' for i in range(num_genes)]
 
